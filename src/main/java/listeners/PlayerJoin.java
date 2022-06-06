d3ae508b146ad8fd20c8db32c1c96347f7dafebc
alessandroDeIturbe <aledei2250@gmail.com>
2022-06-06 12:38:17 +0200

    Updated PlayerJoin class

--- src/main/java/listeners/PlayerJoin.java
+++ src/main/java/listeners/PlayerJoin.java
@@ -1,4 +1,5 @@
 package listeners;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Color;
@@ -11,37 +12,23 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.meta.FireworkMeta;
 
 public class PlayerJoin implements Listener {
-
-
     @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
 
-    public void onPlayerJoin(PlayerJoinEvent event){
-//String  text
+        Player playerJoined = event.getPlayer();
         String oneWelcome = "§c§lHello! Welcome, ";
 
-//Create player
-        Player playerJoined = event.getPlayer();
-
-//FireWork at the spawn of the player
         Firework firework = playerJoined.getPlayer().getWorld().spawn(playerJoined.getPlayer().getLocation(), Firework.class);
         FireworkMeta data = (FireworkMeta) firework.getFireworkMeta();
         data.addEffects(FireworkEffect.builder().withColor(Color.PURPLE).withColor(Color.GREEN).with(FireworkEffect.Type.BALL_LARGE).withFlicker().build());
         data.setPower(1);
         firework.setFireworkMeta(data);
-
-//Title and ActionBar at the spawn player
         playerJoined.sendTitle(ChatColor.RED  + "§c§lUHC", ChatColor.RED + oneWelcome + event.getPlayer().getName(), 40, 100, 1);
-
-//Bar
-
         int online = Bukkit.getOnlinePlayers().size();
-
-
         playerJoined.setPlayerListHeaderFooter(ChatColor.GOLD.toString() + ChatColor.BOLD + "UHC GAMES\n"
                         + ChatColor.GRAY + ChatColor.ITALIC+ "Online: " + ChatColor.ITALIC + ChatColor.GREEN + online
                         + ChatColor.DARK_GREEN+ "/20" + ChatColor.RED
                         + "\n<====================>\n",
-                ChatColor.LIGHT_PURPLE.toString() + ChatColor.ITALIC +"\nby adrytech & archiebold2250");
-
+                ChatColor.LIGHT_PURPLE.toString() + ChatColor.ITALIC +"\nby _AdryTech_ & Archiebold2250");
     }
 }
---
+++ src/main/java/me/archietech/com/uhc/UHC.java
@@ -0,0 +1,17 @@
+package me.archietech.com.uhc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UHC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
