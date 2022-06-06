9df1f83ae3c611cba66114c33d686469b2122d73
alessandroDeIturbe <aledei2250@gmail.com>
2022-06-06 12:42:26 +0200

    Updated UHC.java class

--- src/main/java/me/archietech/com/uhc/UHC.java
+++ src/main/java/me/archietech/com/uhc/UHC.java
@@ -1,17 +1,19 @@
 package me.archietech.com.uhc;
 
+import listeners.PlayerJoin;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class UHC extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
-
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "Plugin UHC ON");
+        getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "Plugin UHC OFF");
     }
 }
